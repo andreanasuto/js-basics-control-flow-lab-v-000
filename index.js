06dd1888d6d63265857daf051c2197cd863f4237
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-26 20:41:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function ternaryCheckCity(city) {
   return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip() {
+function switchOnCharmFromTip(tip) {
 Thank you so much.
 Thank you.
 Bye.
