10fdcb53f5d3a7ca05347ef13838bffd4df204ec
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-26 20:38:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ function scuberGreetingForFeet(distance) {
   if (distance < 400) {
     return "This one is on me!"
   } else if (distance >= 2000 && distance < 2500) {
-
-  }
-
-  else {
-
+    return "I will gladly take your thirty bucks."
+  } else {
+      return "No can do"
   }
 }
