3bd939bc1986c8a900047ad754c76e8ca2e16aea
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-26 20:37:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 function scuberGreetingForFeet(distance) {
   if (distance < 400) {
     return "This one is on me!"
+  } else if (distance >= 2000 && distance < 2500) {
+
+  }
+
+  else {
+
   }
 }
